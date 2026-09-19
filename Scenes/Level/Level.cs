e60e3498d99dd97947b428a7cfe5d95bca9e35f5
nicklefritz <nrcoover@gmail.com>
2026-09-19 10:34:40 -0400

    Programmatically setting tiles on a map
    Demo / prototype

--- Scenes/Level/Level.cs
+++ Scenes/Level/Level.cs
@@ -1,15 +1,30 @@
 using Godot;
-using System;
 
-public partial class Level : Node2D
+public partial class Level : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public enum TileAtlass
 	{
+		primary = 0,
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	[Export] private TileMapLayer _floor;
+	public override void _Ready()
 	{
+		GD.Print(_floor.GetUsedCells());
+		GD.Print(_floor.GetUsedRect());
+		GD.Print(_floor.GetUsedRect().GetCenter());
+		
+		var targetSetLocation = new Vector2I(3,1);
+		var tileAtlassCoordinate = new Vector2I(1,0);
+
+		// We can use the "Atlas Id" of a TileSet's srpitesheet and then the Atlass Coordinate to programmatically select a tile from a tileset for dynamically adding tiles to a map through code.
+		_floor.SetCell(targetSetLocation, (int)TileAtlass.primary, tileAtlassCoordinate);
+
+		var targetEraseLocation = new Vector2I(2,1);
+
+		_floor.EraseCell(targetEraseLocation);
+
+		// The following would clear all tiles from the map:
+		// _floor.Clear();
 	}
 }
